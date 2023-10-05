c4f59cd2cf4becd9bd8116b7cbfd674c7ca7629d
mughilan16 <mughilant@proton.me>
2023-10-05 07:30:55 +0530

    add editor api

--- app/api/editor/route.ts
+++ app/api/editor/route.ts
@@ -1,16 +1,32 @@
-import { Example } from "@/app/types/example"
+import { Example } from "@/app/types/example";
+import data from "../data.json";
+import { writeFile } from "fs";
 
 type Chapter = {
-  title: string,
-  description: string,
-  examples: Array<Example>
-}
+  title: string;
+  description: string;
+  examples: Array<Example>;
+};
 type Payload = {
-  chapter: Chapter
-}
+  chapter: Chapter;
+};
 
 export async function POST(r: Request) {
-  const data: Promise<Chapter> = r.json();
-  console.log(data)
-  return Response.json({ hello: "hello"})
-} 
+  const response: Promise<Payload> = r.json();
+  const chapters = data.chapters;
+  response
+    .then((res) => {
+      return res.chapter;
+    })
+    .then((chapter) => {
+      chapters.push({ ...chapter, id: data.chapters.length });
+      const writeData = JSON.stringify({ chapters: chapters });
+      writeFile("../data.json", writeData, (error) => {
+        if (error) {
+          console.error(error);
+        }
+        console.log("Data.json written correctly");
+      });
+    });
+  return Response.json({ hello: "hello" });
+}
